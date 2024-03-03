ca306dceaf2a9a5a46960354d8f0a305ebff3abe
Hubert Ryan <hubert.ryan@nickelpay.com.br>
2024-03-03 04:19:20 -0300

    feat: compiler removing components without contents

--- lib/content/compilerComponent.ts
+++ lib/content/compilerComponent.ts
@@ -10,15 +10,17 @@ function formatRichText(text: string) {
 export function compilerComponent(components: Component[]) {
   let result: string[] = [];
 
-  components.forEach((item) => {
-    if (item.type == "title") {
-      result.push(`<h2>${item.content}</h2>`);
-    } else if (item.type == "text") {
-      result.push(`<p>${formatRichText(item.content)}</p>`);
-    } else {
-      result.push(`<img src={${item.content}}>`);
-    }
-  });
+  components
+    .filter((component) => component.content != "")
+    .forEach((item) => {
+      if (item.type == "title") {
+        result.push(`<h2>${item.content}</h2>`);
+      } else if (item.type == "text") {
+        result.push(`<p>${formatRichText(item.content)}</p>`);
+      } else {
+        result.push(`<img src={${item.content}}>`);
+      }
+    });
 
   return result;
 }
